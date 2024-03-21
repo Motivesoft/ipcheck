f8d9efb11b386b3ec958178d1e6be72cead0cefa
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-03-21 12:27:37 +0100

    Add a comment

--- ipcheck.py
+++ ipcheck.py
@@ -1,6 +1,11 @@
 import urllib.request
 
 def get_external_ip():
+    """
+    A function that retrieves the external IP address by querying a specific website.
+    No parameters are needed.
+    Returns a string representing the external IP address.
+    """
     site = urllib.request.urlopen("https://ident.me").read().decode('utf8')
     return site
 
